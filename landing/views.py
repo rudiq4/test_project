6a8ae93af8d8753cfbfa85ac0d55095f45f718aa
rudiq4 <rudikvovan@gmail.com>
2017-12-29 01:09:23 +0200

    edit HTML file's / Bootstrap

--- landing/views.py
+++ landing/views.py
@@ -5,5 +5,8 @@ def landing(request):
     return render(request, 'landing/landing.html', locals())
 
 def egg(request):
+    a = 1
+    b = 2
+    suma = a+b
 
     return render(request, 'landing/egg.html', locals())
