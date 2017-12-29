b0eb7bcb265983bc5f6212568251440f3d35cad7
rudiq4 <rudikvovan@gmail.com>
2017-12-29 02:31:04 +0200

    bugfix

--- landing/urls.py
+++ landing/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from landing import views
 
+
 urlpatterns = [
-    url(r'^landing', views.landing, name = 'landing'),
-    url(r'^egg',views.egg, name = 'easter_egg')
+    url(r'^landing', views.landing, name = 'landing')
 ]
