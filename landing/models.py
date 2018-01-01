e2f3bd3cce395910c5ed969ab5c85807bc0797ff
rudiq4 <rudikvovan@gmail.com>
2018-01-01 23:57:52 +0200

    New landing.html,added forms,model into landing ,make migrations , migrate . Stop

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Subsribers(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=128)
--- test_project/settings.py
+++ test_project/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing.apps.LandingConfig',
+    'landing',
 ]
 
 MIDDLEWARE = [
