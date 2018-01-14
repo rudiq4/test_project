0f47af47a97506b968223bc9383a18dc2de76041
rudiq4 <rudikvovan@gmail.com>
2018-01-14 17:51:19 +0200

    The end of lesson #2. Add working form.

--- landing/models.py
+++ landing/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Subscribers(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=11)
-    age = models.IntegerField
 
 
 
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from .forms import SubscriberForm
 
 def landing(request):
 
+    form = SubscriberForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print (data['name'])
+
+        new_form = form.save()
+
+
+
     return render(request, 'landing/landing.html', locals())
+
+
+
+
+
+
+
+
+
+
+
+
 
 def about(request):
 
