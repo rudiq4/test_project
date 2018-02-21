96b4c76525d13955f70e54246ae35468fb42c9c0
rudiq4 <rudikvovan@gmail.com>
2018-02-21 19:45:32 +0200

    git error

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import *
 
 
+class BookTypeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in BookType._meta.fields]
+
+    class Meta:
+        model = BookType
+
+admin.site.register(BookType, BookTypeAdmin)
+
+
+
 class ProductImageInline(admin.TabularInline):  # При добавлянні книги добавляєм йому картинку
     model = ProductImage
     extra = 1  # Тільки 1 поле для завантаження картинки
--- test_project/settings.py
+++ test_project/settings.py
@@ -101,5 +101,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static",),)
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, "static", "static_dev"),
+)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 
